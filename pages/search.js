2a2f3b3ba88035abfe841b9846ee17bc89c13905
am-miracle <judemiraco0@gmail.com>
2022-01-18 15:23:09 +0100

    add search features

--- pages/search.js
+++ pages/search.js
@@ -7,8 +7,9 @@ import { BsFilter } from "react-icons/bs";
 import SearchFilters from "../components/SearchFilters";
 import Property from "../components/Property";
 
+import noresult from '../assets/images/noresult.svg';
 
-const Search = () => {
+const Search = ({properties}) => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
 
@@ -34,11 +35,12 @@ const Search = () => {
                 Properties {router.query.purpose}
             </Text>
             <Flex flexWrap="wrap">
-                {[].map((property) => <Property property={property} key={property.id} />)}
+                {properties.map((property) => <Property property={property} key={property.id} />)}
             </Flex>
-            {[].length === 0 && (
+            {properties.length === 0 && (
                 <Flex justifyContent="center" alignItems="center" flexDirection="column" margintTop="5" marginBottom="5">
                     <Image alt="no result" src={noresult} />
+                    <Text fontSize="2xl" marginTop="3">No Results Found</Text>
                 </Flex>
             )}
         </Box>
@@ -47,3 +49,24 @@ const Search = () => {
 }
 
 export default Search;
+
+export async function getStaticProps({ query }) {
+    const purpose = query.purpose || 'for-rent';
+    const rentFrequency = query.rentFrequency || 'yearly';
+    const minPrice = query.minPrice || '0';
+    const maxPrice = query.maxPrice || '1000000';
+    const roomsMin = query.roomsMin || '0';
+    const bathsMin = query.bathsMin || '0';
+    const sort = query.sort || 'price-desc';
+    const areaMax = query.areaMax || '35000';
+    const locationExternalIDs = query.locationExternalIDs || '5002';
+    const categoryExternalID = query.categoryExternalID || '4';
+    
+    const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+
+    return {
+      props: {
+        properties: data?.hits,
+      }
+    }
+  }
